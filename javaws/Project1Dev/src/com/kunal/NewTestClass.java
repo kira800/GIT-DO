33f352f3fa35f2749c98939fc40917ff07dfe801
kirakunal800 <>
2021-01-22 13:03:42 +0530

    Added NewTestClass.java and Instantiated in TestMain.java

---
+++ javaws/Project1Dev/src/com/kunal/NewTestClass.java
@@ -0,0 +1,7 @@
+package com.kunal;
+
+public class NewTestClass {
+    public NewTestClass() {
+        System.out.println("New Test Class Object created");
+    }
+}
---
+++ javaws/Project1Dev/src/com/kunal/TestMain.java
@@ -0,0 +1,8 @@
+package com.kunal;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println(" Hello Wolrd");
+        NewTestClass nt=new NewTestClass();
+}
+}
